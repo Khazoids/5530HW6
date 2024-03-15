7aeeebc52ddfedad40c8f8f6500d4dba3b471eac
Khazoids <Khazoids@gmail.com>
2024-03-14 21:21:43 -0700

    init commit

---
+++ ChessBrowser/ChessEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    public class ChessEvent
+    {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public DateOnly EventDate { get; set; }
+        public string Site { get; set; }
+        public ChessEvent() { }
+    }
+}
---
+++ ChessBrowser/ChessGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    public class ChessGame
+    {  
+        public string Event { get; set; }
+        public string Round { get; set; }
+        public int WhitePlayer { get; set; }
+        public int BlackPlayer { get; set; }
+        public char Result { get; set; }
+        public string Moves { get; set; }
+
+
+        public ChessGame() { }
+    }
+}
